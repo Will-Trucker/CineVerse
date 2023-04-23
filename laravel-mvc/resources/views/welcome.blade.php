42b3c94d99fe375984cf41ca288f9118af17a7ec
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-04-22 19:43:42 -0600

    footer terminado

--- laravel-mvc/resources/views/welcome.blade.php
+++ laravel-mvc/resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
     <link rel="preload" href="{{asset('css/welcome.css')}}">
     <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/5b41b5f095.js" crossorigin="anonymous"></script>
 </head>
 <body>
   <header>
@@ -28,6 +28,55 @@
       </nav>
   </header>
 
+  <footer>
+    <div class="container">
+        <div class="sec about-us">
+            <h2>Sobre Nosotros</h2>
+            <p>Cineverse es una empresa dedicada a la reserva de asientos de cine que se preocupa por ofrecer la mejor experiencia posible a sus clientes...</p>
+            <ul class="sci">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i>
+                </a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>
+             </ul>
+        </div>
+        <div class="sec quick-links">
+            <h2>Enlaces Utiles</h2>
+            <ul>
+                <li><a href="#">Conócenos</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Terminos y Condiciones</a></li>
+            </ul>
+        </div>
+        <div class="sec contact">
+            <h2>Contáctanos</h2>
+            <ul class="info">
+                <li>
+                    <span><i class="fa-solid fa-location-dot"></i>
+                    </span>
+                    <span> Km 1 1/2 Calle a plan del Pino,
+                        <br> Ciudadela Don Bosco,
+                        <br>Soyapango</span>
+                </li>
+                <li>
+                    <span><i class="fa-solid fa-phone"></i>
+                    </span>
+                    <p><a href="tel:2525-2525"> +503 2525-2525<br></a></p>
+                </li>
+                <li>
+                    <span><i class="fa-solid fa-envelope"></i>
+                    </span>
+                    <p><a href="mailto:cine.versesv@gmail.com"> cine.versesv@gmail.com</a></p>
+                </li>
+            </ul>
+        </div>
+    </div>
+  </footer>
+  <div class="copyrightText">
+    <p>Copyright © 2023 CineVerse. Derechos Reservados</p>
+  </div>
   <script src="{{asset('js/menu.js')}}" type="text/javascript"></script>
 </body>
 </html>
